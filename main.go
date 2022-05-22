04a3eff35b1090a676f5aca465c8a594972ff33b
zenpk <99558412+zenpk@users.noreply.github.com>
2022-05-22 23:46:22 +0800

    copy files from demo project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	initRouter(r)
+
+	r.Run("0.0.0.0:10250") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
