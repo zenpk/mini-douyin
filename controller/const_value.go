78fa4906b2146b88d0d3a01f01d875c72e678658
LISO011212 <1404982867@qq.com>
2022-05-29 22:43:26 +0800

    扩展接口1

--- controller/const_value.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-// 保存一些常量
-
-const IP = "192.168.50.163" // 服务器 IP，本地开发时改成本地的
-const ServerAddr = "http://" + IP + ":10250"
-
-const DBName = "douyin"        // 数据库名
-const DBUserPass = "root:root" // 数据库用户名密码
