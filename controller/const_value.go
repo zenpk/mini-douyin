baf0cd569a77008568327ac66f02127ac1cd35f6
zenpk <99558412+zenpk@users.noreply.github.com>
2022-05-31 21:08:37 +0800

    ip改为服务器ip

--- controller/const_value.go
+++ controller/const_value.go
@@ -2,7 +2,7 @@ package controller
 
 // 保存一些常量
 
-const IP = "192.168.50.163" // 服务器 IP，本地开发时改成本地的
+const IP = "101.43.179.27" // 服务器 IP，本地开发时改成本地的
 const ServerAddr = "http://" + IP + ":10250"
 
 const DBName = "douyin"        // 数据库名
